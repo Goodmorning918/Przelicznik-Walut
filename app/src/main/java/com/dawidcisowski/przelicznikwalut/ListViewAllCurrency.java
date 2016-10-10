62a2614c7f29559dd6a56f839c2d290913a94bef
dawid <dcisowski1@gmail.com>
2016-10-10 09:42:14 +0200

    Amendments Grapfic

--- app/src/main/java/com/dawidcisowski/przelicznikwalut/ListViewAllCurrency.java
+++ app/src/main/java/com/dawidcisowski/przelicznikwalut/ListViewAllCurrency.java
@@ -3,6 +3,11 @@ package com.dawidcisowski.przelicznikwalut;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 
@@ -17,37 +22,89 @@ public class ListViewAllCurrency extends AppCompatActivity {
     private List<CurrencyDescription> currencyDescriptions;
     private DbHelper dbHelper;
 
+    private Button codeSort;
+    private Button nameSort;
+    private Button countrySort;
+
+    private EditText searchEditText;
+
+    private String searchText="";
+
+    private String sortText="code";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
 
+        codeSort=(Button) findViewById(R.id.codeSortButton);
+        nameSort=(Button) findViewById(R.id.nameSortButton);
+        countrySort=(Button) findViewById(R.id.countrySortButton);
+
+        searchEditText=(EditText) findViewById(R.id.searchEditText);
+
         listView = (ListView) findViewById(R.id.listViewCurrency);
 
         fillListViewData();
 
+        codeSort.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                sortText="code";
+                fillListViewData();
+            }
+        });
+
+        nameSort.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                sortText="name";
+                fillListViewData();            }
+        });
+
+        countrySort.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                sortText="country";
+                fillListViewData();            }
+        });
+
+        searchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                searchText=searchEditText.getText().toString();
+                fillListViewData();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 
+
+
+
     private void fillListViewData() {
+        currencyDescriptions = new ArrayList<CurrencyDescription>();
         dbHelper = new DbHelper(getApplicationContext());
         dbHelper.openDataBase();
-        getAllCurrency();
+        if(searchText.equals("")) {
+            currencyCursor = dbHelper.getAllCurrency(sortText);
+        }else{
+            currencyCursor = dbHelper.getAllCurrency(sortText,searchText);
+        }
+        updateCurrencyList();
         ListViewAdapter listViewAdapter = new ListViewAdapter(this, currencyDescriptions);
         listView.setAdapter(listViewAdapter);
     }
-    private void getAllCurrency() {
-        currencyDescriptions = new ArrayList<CurrencyDescription>();
-        currencyCursor = getAllEntriesFromDb();
-        updateCurrencyList();
-    }
 
-    private Cursor getAllEntriesFromDb() {
-        currencyCursor = dbHelper.getAllCurrency();
-        if (currencyCursor != null) {
-            currencyCursor.moveToFirst();
-        }
-        return currencyCursor;
-    }
 
     private void updateCurrencyList() {
         if (currencyCursor != null && currencyCursor.moveToFirst()) {
@@ -55,7 +112,7 @@ public class ListViewAllCurrency extends AppCompatActivity {
                 String name = currencyCursor.getString(0);
                 String code = currencyCursor.getString(1);
                 String country = currencyCursor.getString(2);
-                Double rate = 1/currencyCursor.getDouble(3);
+                Double rate = currencyCursor.getDouble(3);
                 rate=Double.parseDouble(Count.round(rate,".000"));
                 currencyDescriptions.add(new CurrencyDescription(name,code,country,rate));
             } while ((currencyCursor.moveToNext()));
